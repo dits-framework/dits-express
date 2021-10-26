b1907f3dfb3d021785d7dd46d077b5d4bb0546f9
Dave Welch <david@davidwelch.co>
2021-10-26 11:14:23 -0600

    builds

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+import express from 'express'
 import { WebEvent, configureExpress, GET, PUT, POST, DELETE, HTTP } from "./web/express-di";
+
+export const lib = {
+  express
+}
 
 export {
   WebEvent,
